be6e270309d3f56b68e168a40d664897cd9fcf72
NatashaFedorova <dyadyafedor1287@gmail.com>
2022-08-11 00:05:49 +0300

    fix

--- js/task-06.js
+++ js/task-06.js
@@ -3,9 +3,11 @@ const input = document.querySelector('#validation-input');
 const inputValidationHandler = e => {
   input.classList.remove(input.classList.contains('valid') ? 'valid' : 'invalid');
 
-  Number(e.currentTarget.value.length) !== Number(input.dataset.length)
-    ? input.classList.add('invalid')
-    : input.classList.add('valid');
+  if (Number(e.currentTarget.value.length) !== Number(input.dataset.length)) {
+    input.classList.add('invalid');
+  } else {
+    input.classList.add('valid');
+  }
 };
 
 input.addEventListener('blur', inputValidationHandler);
